c6850b9625016265f02373d519e902ae5df3e6ab
serz4862 <sauravkumar4862@gmail.com>
2023-08-30 00:44:24 +0530

    post method

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ const customers = [
     {title : 'Rohan', id:5}
 ] 
 
+// Http methods GET PUT DELETE POST
+
 //Read Request Handlers
 // Display the message when the URL consist of '/'
 app.get('/', (req,res)=>{
@@ -20,8 +22,38 @@ app.get('/', (req,res)=>{
 
 //Display the list of customer when the URL consists of api customers
 app.get('/api/customers',(req,res)=>{
-    
-})
+    res.send(customers);
+});
+
+//Display the Information of specific Customers when the URL consists of api customers.
+app.get('/api/customers/:id', (req, res)=>{
+    const customer = customers.find(c=>c.id === parseInt(req.params.id));
+    //If there is no valid customer id the display an error with the following
+    if(!customer) res.status(404).send(<h2 style= "font-family: Malgun Gothic; color: darkred;"> Ooo .... Not the part of this member.</h2>);
+    res.send(customer);
+});
+// POST method is used to create the data.
+//CREATE Request Handler
+// CREATE New Customer Information
+app.post('/api/customer', (req,res)=>{
+    const { error } = validateCustomer(req.body);
+    if (error){
+        res.status(400).send(error.detail[0].message)
+        return;
+    }
+    //Increment the customer id
+    const customer= {
+        id: customers.length + 1,
+        title: req.body.title
+    };
+    customers.push(customer);
+    res.send(customer);
+});
+
+
+// Update Request Handler
+//Update the EXisting Customer Information
+app.put()
 
 // validate information.
 
